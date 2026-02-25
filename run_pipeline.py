04540431946fc868409b1422244005fc5523748b
javierbernallou <javierbernallou@gmail.com>
2026-02-25 19:38:23 +0100

    Fix import path for flight_prices_ml module

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,7 +4,7 @@ import sys
 # 👇 Añadir raíz del proyecto al path
 sys.path.append(os.path.abspath("."))
 
-from flight_prices_ml.src.ingest import run
+from flight_price_ml.src.ingest import run
 
 if __name__ == "__main__":
     print("Iniciando pipeline de vuelos...")
